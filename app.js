b51d0ac113e08f5346db20099e7a3c87d171b934
Marcelo Tedeschi <marceloftedeschi@gmail.com>
2017-02-17 16:20:01 +0100

    Added 'request' to handle http(s) requests and added a simple route to search spotify by query

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+let request = require('request')
 let express = require('express')
 let app = express()
 
-app.get('/', function(req, res) {
-  res.send('Welcome')
-});
 app.get('/spotify/search', function (req, res) {
   let query = req.query.q;
-  res.json({result: query})
+
+  request(`https://api.spotify.com/v1/search?q=${query}&type=album,track,artist`, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(response.body);
+    } else {
+      res.send(error);
+    }
+  })
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('listening on port 3000!')
 })
