c33012e8880c27768ff97c056610ac95ff3117df
Marcelo Tedeschi <marceloftedeschi@gmail.com>
2017-02-20 23:19:55 +0100

    post route to add track to playlist

--- api/controllers/playlists.js
+++ api/controllers/playlists.js
@@ -17,4 +17,22 @@ router.get('/', function(req, res) {
   });
 });
 
+router.post('/:id/addTrack', function(req, res) {
+  /* request body format
+    {
+      "track": {
+        ...
+      }
+    }
+  */
+  let { track } = req.body;
+  let id = req.params.id;
+
+  playlistsRepository.insert(id, track).then(response => {
+    res.json(response);
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
 module.exports = router
--- api/repositories/playlists.js
+++ api/repositories/playlists.js
@@ -1,6 +1,7 @@
 let mongodb = require('mongodb');
 let MongoClient = mongodb.MongoClient;
 let mongodb_uri = process.env.MONGODB_URI;
+let { ObjectId } = require('mongodb');
 
 module.exports = {
   get() {
@@ -20,6 +21,34 @@ module.exports = {
               resolve(docs);
           });
         };
+      });
+    });
+  },
+
+  insert(id, track) {
+    return new Promise(function(resolve, reject) {
+      MongoClient.connect(mongodb_uri, function(err, db) {
+        if (err) {
+          console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+          console.log('Connection established to', mongodb_uri);
+
+          // TODO validate track contents
+          if (!track) {
+            reject('No valid track in request');
+          } else {
+            db.collection('playlists')
+              .findOneAndUpdate({ _id: ObjectId(id) }, { $addToSet: { tracks: track } }, function(err, r) {
+                if (err) {
+                  console.log('oops -> ', err);
+                  reject(err);
+                }
+                db.close();
+                resolve('success');
+              }
+            );
+          }
+        }
       });
     });
   }
